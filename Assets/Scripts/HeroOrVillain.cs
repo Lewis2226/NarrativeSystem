293d8b1e0e945d1d1547783880537e7cb6f4f41d
Lewis2226 <131185164+Lewis2226@users.noreply.github.com>
2025-04-25 21:29:23 -0600

    Las base de los sistemas principales y pruebas de HeroOrVillan

--- Assets/Scripts/HeroOrVillain.cs
+++ Assets/Scripts/HeroOrVillain.cs
@@ -5,16 +5,104 @@ using UnityEngine;
 public class HeroOrVillain : MonoBehaviour
 {
     // Declaramos una variable del tipo enum
-    public States.playerStates PlayerState;
+   [SerializeField] private States.playerStates PlayerState;
+   [SerializeField] private int GoodPoints;
+   [SerializeField] private int BadPoints;
+   [SerializeField] private string EventChoosen;
 
     void Start()
     {
         PlayerState = States.playerStates.Neutral;
-        Debug.Log($"El estado del jugador es {PlayerState}");
+
     }
 
-    void Update()
+    public void SetState(States.playerStates NewState)
     {
+        PlayerState = NewState;
+    }
 
+   public States.playerStates GetState()
+   {
+        return PlayerState;
+   }
+
+   public void SetPoints(States.playerStates PointsType, int amount)
+   {
+        if(PointsType == States.playerStates.Good)
+        {
+            GoodPoints += amount;
+        }
+        else
+        {
+            BadPoints += amount;
+        }
+   }
+
+   public int GetPoints(States.playerStates PointsType)
+   {
+        if (PointsType == States.playerStates.Good)
+        {
+            return GoodPoints;
+        }
+        else
+        {
+            return BadPoints;
+        }
+   }
+
+   public string ShowNarrative(int NumberEvent)
+   {
+        string EventChoosen;
+        string EventToShow;
+        switch(NumberEvent)
+        {
+            case 0:
+                if(PlayerState == States.playerStates.Good)
+                {
+                    EventChoosen = "Hola, que hace?";
+                    
+                }
+                else
+                {
+                    EventChoosen = "No me hables";
+                    
+                }
+                EventToShow = EventChoosen;
+                break;
+
+            case 1:
+                if (PlayerState == States.playerStates.Good)
+                {
+                    EventChoosen = "No puede ser";
+                    
+                }
+                else
+                {
+                    EventChoosen = "Alejate , por favor";
+                    
+                }
+                EventToShow = EventChoosen;
+                break;
+
+            case 2:
+                if (PlayerState == States.playerStates.Good)
+                {
+                    EventChoosen = "Adios";
+                    
+                }
+                else
+                {
+                    EventChoosen = "Largo de aqui";
+                    
+                }
+                EventToShow = EventChoosen;
+                break;
+
+            default:
+                EventToShow = "No existe ese evento";
+                break;
+        }
+
+        return "Se eligió este evento " + EventToShow;
     }
 }
