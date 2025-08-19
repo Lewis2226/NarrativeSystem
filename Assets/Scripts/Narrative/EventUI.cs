410190491c9fedb4727ec4510c509060da92dda2
Lewis2226 <ivanrobleslazaro@gmail.com>
2025-08-18 19:12:46 -0600

    Método de mostrar eventos en el menú

---
+++ Assets/Scripts/Narrative/EventUI.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using TMPro;
+
+public class EventUI : MonoBehaviour
+{
+    public Transform contentPanel;
+    public GameObject eventPanelPrefab;
+    public EventUI Instance { get; private set; }
+
+   public void Awake()
+   {
+    if (Instance != null && Instance != this)
+    {
+        Destroy(this);
+    }
+    else
+    {
+        Instance = this;
+        DontDestroyOnLoad(this);
+    }
+   }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        RefreseUI();
+    }
+
+    public void RefreseUI()
+    {
+        foreach (Transform child in contentPanel)
+        {
+            Destroy(child.gameObject);
+        }
+
+        if (EventController.Instance.activedEvents != null)
+        {
+            foreach( Events events in EventController.Instance.activedEvents)
+            {
+                GameObject newEvent = Instantiate(eventPanelPrefab, contentPanel);
+                TMP_Text text = newEvent.GetComponent<TMP_Text>();
+                text.text = events.name;
+            }
+        }
+    }
+}
